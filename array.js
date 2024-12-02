6d4260066cb47249934e0f8a556df5277d390fb5
harshity9 <120360379+harshity9@users.noreply.github.com>
2024-12-02 08:57:31 +0000

    v5

--- array.js
+++ array.js
@@ -1,23 +1,34 @@
-const array = [1,2, 4, 5, 6, 56, 4, 32, 443]
-console.log(array)
+// const array = [1,2, 4, 5, 6, 56, 4, 32, 443]
+// console.log(array)
 
-array.push(79)
-console.log(array)
+// array.push(79)
+// console.log(array)
 
-array.pop()
-console.log(array)
+// array.pop()
+// console.log(array)
 
-array.pop()
-console.log(array)
+// array.pop()
+// console.log(array)
 
-console.log(array.includes(56))
+// console.log(array.includes(56))
 
-console.log(typeof(array.join()))
+// console.log(typeof(array.join()))
 
-const arr1 = array.slice(2, 4)
-console.log('A  '+arr1)
-console.log(array)
+// const arr1 = array.slice(2, 4)
+// console.log('A  '+arr1)
+// console.log(array)
 
-console.log('sdvs'+array.splice(1,4))
-console.log(array)
+// console.log('sdvs'+array.splice(1,4))
+// console.log(array)
+
+
+const array2 = ['efsdf', 'dsfvsdf', 'fvdfvdfvdf']
+const array1 = ['hrlo', 'skjfkj', 'dfnvkj']
+const array0= ['rdgrt','rgvdrf0','rgtrtgvrd']
+
+const array3 = array1.concat(array2)
+console.log(array3)
+
+const arr2 = [...array0, ...array1, ...array2]
+console.log(arr2)
 
