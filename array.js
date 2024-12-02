4436d32000843afe7a9225ca9e1256e169c1ccda
harshity9 <120360379+harshity9@users.noreply.github.com>
2024-12-02 08:44:20 +0000

    v5

---
+++ array.js
@@ -0,0 +1,23 @@
+const array = [1,2, 4, 5, 6, 56, 4, 32, 443]
+console.log(array)
+
+array.push(79)
+console.log(array)
+
+array.pop()
+console.log(array)
+
+array.pop()
+console.log(array)
+
+console.log(array.includes(56))
+
+console.log(typeof(array.join()))
+
+const arr1 = array.slice(2, 4)
+console.log('A  '+arr1)
+console.log(array)
+
+console.log('sdvs'+array.splice(1,4))
+console.log(array)
+
