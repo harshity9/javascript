414d938a4a11b8ed31df87ab3f08e66ac7c5d170
harshity9 <120360379+harshity9@users.noreply.github.com>
2024-12-02 12:49:51 +0000

    v8

--- object.js
+++ object.js
@@ -1,28 +1,50 @@
-const sym = Symbol("hello")
+// const sym = Symbol("hello")
 
-const obj = {
-    name: 'harshit',
-    [sym]: "hii",
-    age: '20',
-    field: "comp sci",
-    email: "hasr@gmail.com",
-    time: "Notime"
+// const obj = {
+//     name: 'harshit',
+//     [sym]: "hii",
+//     age: '20',
+//     field: "comp sci",
+//     email: "hasr@gmail.com",
+//     time: "Notime"
+// }
+// console.log(obj["email"])
+
+// obj.name = ("ranodm1")
+
+// // Object.freeze(obj)
+// // obj.name = ("dsuhncsukj")
+
+// // console.log(obj)
+// const today = new Date()
+// const livetime = today.toLocaleString('default',{
+//     timeZone: 'IST'
+// })
+
+// obj.date = function(){
+//     console.log(`Hello there, I am ${this.name} and live time is ${livetime}`)
+// }
+
+// console.log(obj.date())
+
+const obj1 = {
+    a:'1',
+    b:'2',
+    c:'3',
+    d:'4',
 }
-console.log(obj["email"])
 
-obj.name = ("ranodm1")
-
-// Object.freeze(obj)
-// obj.name = ("dsuhncsukj")
-
-// console.log(obj)
-const today = new Date()
-const livetime = today.toLocaleString('default',{
-    timeZone: 'IST'
-})
-
-obj.date = function(){
-    console.log(`Hello there, I am ${this.name} and live time is ${livetime}`)
+const obj2= {
+    e:'5',
+    f:'6',
+    g:'7',
+    h:'8',
 }
 
-console.log(obj.date())
+// obj3 = { ...obj1, ...obj2}
+// console.log(obj3);
+
+// console.log(Object.values(obj2))
+
+const {f:m} = obj2
+console.log(m)
