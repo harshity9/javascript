13b24b5ec6768ebad70dfc54dcb79d243c5b9bb4
harshity9 <120360379+harshity9@users.noreply.github.com>
2024-12-03 09:27:09 +0000

    v9

---
+++ function.js
@@ -0,0 +1,12 @@
+function addition (num1, num2){
+    if (num1>99){
+        return `very large number`
+    }
+     let res = num1+num2
+    return res
+}
+
+// if there will be no return it won't return any value
+
+const result = addition(66,4);
+console.log(result)
