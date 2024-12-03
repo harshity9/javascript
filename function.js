0b837abe521a46dc8607e30adebc786a4c1a15e2
harshity9 <120360379+harshity9@users.noreply.github.com>
2024-12-03 10:47:32 +0000

    v10

--- function.js
+++ function.js
@@ -1,12 +1,35 @@
-function addition (num1, num2){
-    if (num1>99){
-        return `very large number`
-    }
-     let res = num1+num2
-    return res
+// function addition (num1, num2){
+//     if (num1>99){
+//         return `very large number`
+//     }
+//      let res = num1+num2
+//     return res
+// }
+
+// // if there will be no return it won't return any value
+
+// const result = addition(66,4);
+// console.log(result)
+
+const obj1 = {
+    name: 'harshit',
+    class: '1',
+    roll_no: '23',
 }
 
-// if there will be no return it won't return any value
+function studetail (anyobject){
+    return `hello i am ${anyobject.name} and i study in class ${anyobject.class}.`
+}
+console.log(studetail(obj1))
 
-const result = addition(66,4);
-console.log(result)
+
+let arr1 = [ "abc","def","ghi","jkl" ]
+
+function passingarray (anyarray){
+    return anyarray 
+}
+
+console.log(passingarray(arr1))
+
+
+
