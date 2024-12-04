361d4b25c4be5d87535833ce4db2df9aa199ac5c
harshity9 <120360379+harshity9@users.noreply.github.com>
2024-12-04 11:47:13 +0000

    v12

--- loop.js
+++ loop.js
@@ -1,8 +1,8 @@
-for (let i = 0; i <= 10; i++) {
-    const element = i;
-    for (let j = 0; j <=10; j++) {
-        const element1 = j;
-        console.log(element1)
-}
-    console.log(element)
-}
+// for (let i = 0; i <= 10; i++) {
+//     const element = i;
+//     for (let j = 0; j <=10; j++) {
+//         const element1 = j;
+//         console.log(element1)
+// }
+//     console.log(element)
+// }
