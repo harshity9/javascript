77bad8087cd73c721ef8af7137099ef60a1cca3d
harshity9 <120360379+harshity9@users.noreply.github.com>
2024-12-04 09:11:50 +0000

    v10

--- function.js
+++ function.js
@@ -11,25 +11,45 @@
 // const result = addition(66,4);
 // console.log(result)
 
-const obj1 = {
-    name: 'harshit',
-    class: '1',
-    roll_no: '23',
-}
+// const obj1 = {
+//     name: 'harshit',
+//     class: '1',
+//     roll_no: '23',
+//     welcome: function (){
+//         console.log(`${this.class} great` )
+//         console.log(this)
+//     }
+// }
+// obj1.welcome();
 
-function studetail (anyobject){
-    return `hello i am ${anyobject.name} and i study in class ${anyobject.class}.`
-}
-console.log(studetail(obj1))
+// console.log(this)
 
-
-let arr1 = [ "abc","def","ghi","jkl" ]
-
-function passingarray (anyarray){
-    return anyarray 
-}
-
-console.log(passingarray(arr1))
+// function studetail (anyobject){
+//     return `hello i am ${anyobject.name} and i study in class ${anyobject.class}.`
+// }
+// console.log(studetail(obj1))
 
 
+// let arr1 = [ "abc","def","ghi","jkl" ]
+
+// function passingarray (anyarray){
+//     return anyarray 
+// }
+
+// console.log(passingarray(arr1))
+
+
+
+// arrow function
+(function arrowf(num,num1){
+    const result =  console.log(num + num1)
+    return result
+ })(3223,321);    
+//  this is iife
+
+
+const addd = (numm, numm2) => 
+    ( numm + numm2)
+
+console.log(addd(223,23232))
 
