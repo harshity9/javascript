a0e76f48385af44c6f1d036ad725863011606cc2
harshity9 <120360379+harshity9@users.noreply.github.com>
2024-12-05 08:36:14 +0000

    v112

--- loop.js
+++ loop.js
@@ -30,24 +30,39 @@
 // })
 // console.log(mynumber)
 
-var library = [ 
-   {
-       author: 'Bill Gates',
-       title: 'The Road Ahead',
-       readingStatus: true
-   },
-   {
-       author: 'Steve Jobs',
-       title: 'Walter Isaacson',
-       readingStatus: true
-   },
-   {
-       author: 'Suzanne Collins',
-       title:  'Mockingjay: The Final Book of The Hunger Games', 
-       readingStatus: false
-   }];
+// var library = [ 
+//    {
+//        author: 'Bill Gates',
+//        title: 'The Road Ahead',
+//        readingStatus: true
+//    },
+//    {
+//        author: 'Steve Jobs',
+//        title: 'Walter Isaacson',
+//        readingStatus: true
+//    },
+//    {
+//        author: 'Suzanne Collins',
+//        title:  'Mockingjay: The Final Book of The Hunger Games', 
+//        readingStatus: false
+//    }];
 
-   const retbooks = library.filter((book)=>{
-      return (book.readingStatus === true)
-   })
-   console.log(retbooks)
+//    const retbooks = library.filter((book)=>{
+//       return (book.readingStatus === true || book.author === 'Suzanne Collins')
+//    })
+//    console.log(retbooks)
+
+// const mynum =  [12, 43,23,322,12,32,43,543,432,212,43254,6,6544,34,354,623 ]
+
+// const newnum = mynum.map((num ) =>{
+//     return (num+ 1)
+// }).filter((num)=>{
+//     return (num%3 == 0)
+// })
+// console.log(newnum)
+const mynum = [12, 43, 56, 12 ,15, 32]
+
+const number1 = mynum.reduce((acc, crv)=>{
+    return (acc+crv)
+}, 0)
+console.log(number1)
