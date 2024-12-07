9624c4e3623532ad9de5957e8393fb03d1ca4846
harshit <harshityadav0927@gmail.com>
2024-12-07 15:39:44 +0530

    v35

--- projects/color.js
+++ projects/color.js
@@ -4,5 +4,6 @@ buttons.forEach(function(event){
     (addEventListener('click',function(e){
         const setColor = (e.target.id)
         document.querySelector('body').style.backgroundColor = setColor
+        
     }))
 })
